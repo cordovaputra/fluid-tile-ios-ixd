b02c5d37a00a2d6742fbf992e54d8f97a59369e1
cordovaputra <cordovaputra.handri@gmail.com>
2023-12-12 16:53:25 +0700

    Initial Commit

---
+++ Fluid-Tile-IXD/Fluid_Tile_IXDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_Tile_IXDApp.swift
+//  Fluid-Tile-IXD
+//
+//  Created by Cordova  on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Fluid_Tile_IXDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
